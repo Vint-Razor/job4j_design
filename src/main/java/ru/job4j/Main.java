8f8fbdae578a8cbee9b4b396cce89e915500cd33
Рустам <vint-razor@yandex.ru>
2022-01-06 13:46:43 +0500

    jar build

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,7 +1,11 @@
 package ru.job4j;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
     }
 }

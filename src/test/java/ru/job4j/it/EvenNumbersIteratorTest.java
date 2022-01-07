4d6361cc29948d18101eedcd9f13f56cbba2dc1f
Vint_Razor <vint-razor@yandex.ru>
2022-01-07 17:11:10 +0500

    5.1.2. Создать итератор четные числа [#150]

---
+++ src/test/java/ru/job4j/it/EvenNumbersIteratorTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.it;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class EvenNumbersIteratorTest {
+
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp() {
+        it = new EvenNumbersIterator(new int[] {1, 2, 3, 4, 5, 6, 7});
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldReturnEvenNumbersSequentially() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        it.next();
+    }
+
+    @Test
+    public void shouldReturnFalseIfNoAnyEvenNumbers() {
+        it = new EvenNumbersIterator(new int[] {1});
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void allNumberAreEven() {
+        it = new EvenNumbersIterator(new int[] {2, 4, 6});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+    }
+}

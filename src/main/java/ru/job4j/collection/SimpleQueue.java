832627ad145d541f95594054d3e73560dd8ac0f3
Рустам <vint-razor@yandex.ru>
2022-02-16 10:49:32 +0500

    5. Очередь на двух стеках [#160]

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.NoSuchElementException;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+
+    public T poll() {
+        if (out.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return out.pop();
+    }
+
+    public void push(T value) {
+        in.push(value);
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+    }
+}

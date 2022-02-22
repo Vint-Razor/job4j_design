78a6249a5938d335684414f442ca105590b6654a
Рустам <vint-razor@yandex.ru>
2022-02-22 15:47:35 +0500

    0. Hash структуры [#4955]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -4,8 +4,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        Scanner scanner = new Scanner(System.in);
+        String str = "hello world";
+        System.out.println(str);
+        System.out.println(binary(123));
+    }
+
+    public static void hashPrint(String str, Scanner scanner) {
         int num = scanner.nextInt();
+        str = num + " " + str;
+        System.out.println(str + " " + str.hashCode());
+    }
+
+    public static String binary(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 31; i++) {
+            sb.append(num % 2 == 0 ? 0 : 1);
+            sb.append((i + 1) % 8 == 0 ? " " : "");
+            num /= 2;
+        }
+        return sb.reverse().toString();
     }
 }

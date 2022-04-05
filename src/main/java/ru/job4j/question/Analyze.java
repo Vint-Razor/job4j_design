46043e01620be864bcee2bfe5fa64b0229cee4f4
Рустам <vint-razor@yandex.ru>
2022-04-05 13:06:10 +0500

    2. Статистика по коллекции. [#45889] - каркас

---
+++ src/main/java/ru/job4j/question/Analyze.java
@@ -0,0 +1,11 @@
+package ru.job4j.question;
+
+import java.util.Set;
+
+public class Analyze {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        Info info = new Info(0, 0, 0);
+        return info;
+    }
+}

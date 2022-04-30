da98ce5d5686996b299dc3f7179491a2d9732187
Vint_Razor <vint-razor@yandex.ru>
2022-04-30 10:39:25 +0500

    0.2. FileInputStream [#4898]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,16 +1,23 @@
 package ru.job4j.io;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
         try (FileInputStream in = new FileInputStream("even.txt")) {
-            int num;
-            while ((num = in.read()) != -1) {
+            int read;
+            StringBuilder text = new StringBuilder();
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                int num = Integer.parseInt(line);
                 boolean rsl = num % 2 == 0;
                 System.out.println(rsl);
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }

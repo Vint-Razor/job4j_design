6b104cbb41a94717bbf162ab1a326a0a106123a2
Рустам <vint-razor@yandex.ru>
2022-05-06 16:54:21 +0500

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Analizy {
+    public void unavailable(String source, String target) {
+        StringBuilder sb = new StringBuilder();
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            boolean error = false;
+            for (String[] arr : readToList(source)) {
+                if (!error && Integer.parseInt(arr[0]) >= 400) {
+                    sb.append(arr[1]);
+                    sb.append(";");
+                    error = true;
+                } else if (error && Integer.parseInt(arr[0]) < 400) {
+                    sb.append(arr[1]);
+                    error = false;
+                    out.println(sb);
+                    sb.setLength(0);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<String[]> readToList(String source) {
+        LinkedList<String[]> list = new LinkedList<>();
+        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
+            read.lines()
+                    .map(str -> str.split(" "))
+                    .forEach(list::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Analizy analizy = new Analizy();
+        analizy.unavailable("server.log", "target.csv");
+    }
+}

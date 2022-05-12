cb87791f022a6d1595803d78faca5e78816ccf0b
Vint_Razor <vint-razor@yandex.ru>
2022-05-12 21:49:23 +0500

    4.1. Сканирование файловой системы. [#106929]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,12 +10,12 @@ import java.util.function.Predicate;
 public class Search {
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".csv")).forEach(System.out::println);
+        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
-        SearchFiles search = new SearchFiles(condition);
-        Files.walkFileTree(root, search);
-        return search.getPath();
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPath();
     }
 }

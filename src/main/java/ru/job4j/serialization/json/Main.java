0bdfd96304922bec20d6a632fad4cf790b859b13
Vint_Razor <vint-razor@yandex.ru>
2022-06-06 20:04:34 +0500

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/json/Main.java
+++ src/main/java/ru/job4j/serialization/json/Main.java
@@ -7,24 +7,28 @@ import javax.xml.bind.Unmarshaller;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws JAXBException, IOException {
-        final Warrior warrior = new Warrior("Jack", 20, false,
-                new Weapon("Sword", 3), new String[]{"berserk", "slow"});
+        Path path = Path.of("./data/serialization/warrior.xml");
+        Warrior warrior;
+        String xml = Files.readString(path);
         JAXBContext context = JAXBContext.newInstance(Warrior.class);
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        String xml = "";
+        System.out.format("десериализация из файла %s%n", path.toAbsolutePath());
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            warrior = (Warrior) unmarshaller.unmarshal(reader);
+            System.out.println(warrior);
+        }
+        System.out.format("сереализация из обекта %s%n", warrior.getClass());
         try (StringWriter writer = new StringWriter()) {
             marshaller.marshal(warrior, writer);
             xml = writer.getBuffer().toString();
             System.out.println(xml);
-        }
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        try (StringReader reader = new StringReader(xml)) {
-            Warrior result = (Warrior) unmarshaller.unmarshal(reader);
-            System.out.println(result);
         }
     }
 }

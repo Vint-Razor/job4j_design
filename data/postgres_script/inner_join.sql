94fa1c87e32f799af27b9b078e35a9fadd388216
Рустам <vint-razor@yandex.ru>
2022-08-03 17:10:10 +0500

    1. Inner join [#373617 #328418]

--- data/postgres_script/inner_join.sql
+++ data/postgres_script/inner_join.sql
@@ -1,3 +1,25 @@
+create table unit_class
+(
+	id serial primary key,
+	"class" varchar(255)
+);
+create table unit
+(
+	id serial primary key,
+	"name" varchar(255),
+	unit_class_id int references unit_class(id)
+);
+insert into unit_class("class") values ('warrior');
+insert into unit_class("class") values ('archer');
+insert into unit_class("class") values ('mage');
+insert into unit("name", unit_class_id) values('John', 1);
+insert into unit("name", unit_class_id) values('Bill', 1);
+insert into unit("name", unit_class_id) values('Sam', 2);
+insert into unit("name", unit_class_id) values('Robin', 2);
+insert into unit("name", unit_class_id) values('Veniamin', 3);
+insert into unit("name") values('Karl');
+insert into unit("name") values('Bob');
+
 select u.name, c.class
 from unit as u join unit_class as c on u.unit_class_id = c.id;
 
@@ -6,3 +28,5 @@ from unit as u join unit_class as c on u.unit_class_id = c.id;
 
 select u.name "Имя персонажа", c.class "Класс персонажа"
 from unit u join unit_class c on u.unit_class_id = c.id;
+
+select * from unit;

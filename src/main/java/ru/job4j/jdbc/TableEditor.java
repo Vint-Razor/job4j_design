8e18feb662d99c2ca7f057b45d9816d45ba08cdb
Vint_Razor <vint-razor@yandex.ru>
2022-08-17 23:43:55 +0500

    0.1. Statement [#379306] - написал методы createTable, addColumn

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -2,16 +2,14 @@ package ru.job4j.jdbc;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 import java.util.StringJoiner;
 
 public class TableEditor implements AutoCloseable {
 
     private Connection connection;
-    private Properties properties;
+    private final Properties properties;
 
     public TableEditor(Properties properties) {
         this.properties = properties;
@@ -36,7 +34,12 @@ public class TableEditor implements AutoCloseable {
     }
 
     public void createTable(String tableName) {
-
+        try (Statement statement = connection.createStatement()) {
+            var sql = String.format("create table if not exists %s();", tableName);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropTable(String tableName) {
@@ -44,7 +47,13 @@ public class TableEditor implements AutoCloseable {
     }
 
     public void addColumn(String tableName, String columnName, String type) {
-
+        try (Statement statement = connection.createStatement()) {
+            var sql = String.format("ALTER TABLE %s ADD COLUMN %s %s;",
+                    tableName, columnName, type);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropColumn(String tableName, String columnName) {
@@ -55,14 +64,14 @@ public class TableEditor implements AutoCloseable {
 
     }
 
-    public static String getTableScheme(Connection connection, String tableName) throws SQLException {
+    public static String getTableScheme(Connection connection, String tableName) {
         var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
         var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
         var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
         buffer.add(header);
         try (var statement = connection.createStatement()) {
             var selection = statement.executeQuery(String.format(
-                    "select * from %s limit 1", tableName
+                    "select * from %s limit 1;", tableName
             ));
             var metaData = selection.getMetaData();
             for (int i = 1; i <= metaData.getColumnCount(); i++) {
@@ -70,6 +79,8 @@ public class TableEditor implements AutoCloseable {
                         metaData.getColumnName(i), metaData.getColumnTypeName(i)
                 ));
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return buffer.toString();
     }
@@ -83,12 +94,15 @@ public class TableEditor implements AutoCloseable {
 
     public static void main(String[] args) {
         Properties properties = new Properties();
-        try (InputStream in = TableEditor.class.getClassLoader().getResourceAsStream("postgres.properties")) {
+        ClassLoader loader = TableEditor.class.getClassLoader();
+        try (InputStream in = loader.getResourceAsStream("postgres.properties")) {
             properties.load(in);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(properties.getProperty("hibernate.connection.url"));
         TableEditor tableEditor = new TableEditor(properties);
+        tableEditor.createTable("create_test");
+        tableEditor.addColumn("create_test", "name", "varchar(255)");
+        System.out.println(getTableScheme(tableEditor.connection, "create_test"));
     }
 }

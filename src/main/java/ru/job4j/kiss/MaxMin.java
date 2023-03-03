afe4bcf983c598f53d33dc756d7d519967a3a460
Home <vint-razor@yandex.ru>
2023-03-03 22:29:39 +0500

    2. Принципы Kiss, Dry и Yagni [#238813 #396953]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -2,16 +2,25 @@ package ru.job4j.kiss;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class MaxMin {
 
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        value.sort(comparator);
-        return value.get(value.size() - 1);
+        Predicate<Integer> predicate = (x -> x < 0);
+        return loop(value, comparator, predicate);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        value.sort(comparator);
-        return value.get(0);
+        Predicate<Integer> predicate = (x -> x > 0);
+        return loop(value, comparator, predicate);
+    }
+
+    private <T> T loop(List<T> values, Comparator<T> comparator, Predicate<Integer> predicate) {
+        T result = values.get(0);
+        for (T val : values) {
+            result = predicate.test(comparator.compare(result, val)) ? val : result;
+        }
+        return result;
     }
 }
--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -9,12 +9,12 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MaxMinTest {
-    private final List<Integer> list = Arrays.asList(1, 5, 3);
+    private final List<Integer> list = Arrays.asList(1, 4, 5, 3, 7, 8, 6);
     private final MaxMin maxMin = new MaxMin();
 
     @Test
     void checkMax() {
-        Integer actual = 5;
+        Integer actual = 8;
         var expected = maxMin.max(list, Comparator.naturalOrder());
         assertThat(actual).isEqualTo(expected);
     }

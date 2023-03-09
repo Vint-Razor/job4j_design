b33a5df30ab239737de89ee114344de0f10de416
Home <vint-razor@yandex.ru>
2023-03-09 13:29:16 +0500

    1. Отчеты. [#850] - каркас

---
+++ src/test/java/ru/job4j/odd/srp/report/ReportEngineTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.odd.srp.report;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.odd.srp.formatter.DateTimeParser;
+import ru.job4j.odd.srp.formatter.ReportDateTimeParser;
+import ru.job4j.odd.srp.model.Employee;
+import ru.job4j.odd.srp.store.MemStore;
+
+import java.util.Calendar;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ReportEngineTest {
+
+    @Test
+    void whenOldGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
+        store.add(worker);
+        Report engine = new ReportEngine(store, parser);
+        StringBuilder expected = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(" ")
+                .append(parser.parse(worker.getHired())).append(" ")
+                .append(parser.parse(worker.getFired())).append(" ")
+                .append(worker.getSalary())
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true)).isEqualTo(expected.toString());
+    }
+}

ac1b8ee9226733df379b41b27837c1e7622e7345
Home <vint-razor@yandex.ru>
2023-03-13 20:56:23 +0500

    0. Принцип открытости закрытости [#4914] - поле не абстрактный класс

---
+++ src/main/java/ru/job4j/odd/ocp/Warrior.java
@@ -0,0 +1,12 @@
+package ru.job4j.odd.ocp;
+
+public class Warrior {
+
+    private String name;
+    private Weapon weapon;
+
+    public Warrior(String name, Weapon weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+}

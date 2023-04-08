06f1c46ab77b0b83054f641112172d5fb76f5698
Home <vint-razor@yandex.ru>
2023-04-08 19:40:16 +0500

    2. Парковка машин [#853] - добавил два теста 1.размер трака 2. поле припаркованости

--- src/test/java/ru/job4j/odd/lsp/parking/AutoParkingTest.java
+++ src/test/java/ru/job4j/odd/lsp/parking/AutoParkingTest.java
@@ -2,20 +2,22 @@ package ru.job4j.odd.lsp.parking;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Disabled
 class AutoParkingTest {
     private final int carsCapacity = 10;
     private final int trucksCapacity = 5;
     private final Parking autoParking = new AutoParking(carsCapacity, trucksCapacity);
-    private final Auto car = new Car("123");
-    private final Auto car2 = new Car("234");
-    private final Auto truck = new Truck("2d3");
-    private final Auto truck2 = new Truck("4d3");
+    private final Auto car = new Car("car1", 1);
+    private final Auto car2 = new Car("car2", 1);
+    private final Auto truck = new Truck("truck1", 2);
+    private final Auto truck2 = new Truck("truck2", 3);
 
     @BeforeEach
     void before() {
@@ -71,6 +73,18 @@ class AutoParkingTest {
         Auto truck3 = new Truck("5d3");
         autoParking.addAuto(car);
         assertThat(autoParking.addAuto(truck3)).isTrue();
+    }
+
+    @Test
+    void whenAddTrackSizeLess2ThenExcept() {
+        Auto wrongTruck = new Truck("wrong truck", 1);
+        assertThat(autoParking.addAuto(wrongTruck)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenAutoIsParkedThenTrue() {
+        boolean expected = car.isParked();
+        assertThat(expected).isTrue();
     }
 
     @AfterEach
